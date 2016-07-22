f9e4e6b28076b906fa0e36a4e978a08f4c350ae6
devcartha <chaouechi.souhail@gmail.com>
2016-07-22 15:12:50 +0100

    Using CardView as a RecyclerView list item

--- app/src/main/java/tn/devcartha/androidrecyclerview/QuotesAdapter.java
+++ app/src/main/java/tn/devcartha/androidrecyclerview/QuotesAdapter.java
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -37,7 +38,7 @@ public class QuotesAdapter extends RecyclerView.Adapter<QuotesAdapter.QuotesHold
 
     @Override
     public QuotesAdapter.QuotesHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = inflater.inflate(R.layout.list_item, parent, false);
+        View view = inflater.inflate(R.layout.card_item, parent, false);
         return new QuotesHolder(view);
     }
 
@@ -46,16 +47,11 @@ public class QuotesAdapter extends RecyclerView.Adapter<QuotesAdapter.QuotesHold
         QuotesListItem item = listData.get(position);
         holder.title.setText(item.getTitle());
         holder.subTitle.setText(item.getSubTitle());
-        if (item.isFavourite()){
+        /*if (item.isFavourite()){
             holder.secondaryIcon.setImageResource(R.drawable.ic_star_black_24dp);
         } else {
             holder.secondaryIcon.setImageResource(R.drawable.ic_star_border_black_24dp);
-        }
-    }
-
-    @Override
-    public int getItemCount() {
-        return listData.size();
+        }*/
     }
 
     public void setListData(ArrayList<QuotesListItem> exerciseList) {
@@ -63,33 +59,39 @@ public class QuotesAdapter extends RecyclerView.Adapter<QuotesAdapter.QuotesHold
         this.listData.addAll(exerciseList);
     }
 
+    @Override
+    public int getItemCount() {
+        return listData.size();
+    }
+
     class QuotesHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
         ImageView thumbnail;
-        ImageView secondaryIcon;
+        //ImageView secondaryIcon;
         TextView title;
         TextView subTitle;
         View container;
+        Button load;
 
         public QuotesHolder(View itemView) {
             super(itemView);
             thumbnail = (ImageView)itemView.findViewById(R.id.im_item_icon);
-            secondaryIcon = (ImageView)itemView.findViewById(R.id.im_item_icon_secondary);
-            secondaryIcon.setOnClickListener(this);
+            // secondaryIcon = (ImageView)itemView.findViewById(R.id.im_item_icon_secondary);
+            // secondaryIcon.setOnClickListener(this);
             subTitle = (TextView)itemView.findViewById(R.id.lbl_item_sub_title);
             title = (TextView)itemView.findViewById(R.id.lbl_item_text);
             container = (View)itemView.findViewById(R.id.cont_item_root);
-            container.setOnClickListener(this);
+            load = (Button)itemView.findViewById(R.id.btn_card_load);
+            load.setOnClickListener(this);
         }
 
         @Override
         public void onClick(View v) {
-            if (v.getId() == R.id.cont_item_root){
+            if (v.getId() == R.id.btn_card_load){
                 itemClickCallback.onItemClick(getAdapterPosition());
             } else {
-                itemClickCallback.onSecondaryIconClick(getAdapterPosition());
+                // itemClickCallback.onSecondaryIconClick(getAdapterPosition());
             }
         }
-
     }
 }
